7d049f765a149e6954ce4237b39e5d5c411ad4b2
Eliab Parra <eliabparra@gmail.com>
2026-02-07 14:04:00 -0400

    feat(db): introspection support for data in manual tables

---
+++ scripts/db/schemas/90_data_security_profiles.ts
@@ -0,0 +1,8 @@
+/**
+ * Auto-generated data for security.profiles
+ * Generated at: 2026-02-07T18:03:04.751Z
+ */
+export const DATA_PROFILES_SCHEMA = [
+    `INSERT INTO security.profiles (id, name) VALUES ('1', 'public');`,
+    `INSERT INTO security.profiles (id, name) VALUES ('2', 'supervisor');`,
+]
